5e960f34abd3b489dcad3a851724b2a2dfad90a7
KYIV\Anton_Karpenko <anton_karpenko@epam.com>
2017-11-09 12:23:39 +0200

    added  node_files gulpfile.js package.json

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+let gulp = require('gulp'),
+    scss = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('scss', function () {
+    gulp.src('app/scss/style.scss')
+        .pipe(scss())
+        .pipe(gulp.dest('app/css'))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+})
+
+gulp.task('runBrSynk', function () {
+    browserSync.init({
+        server: {
+            baseDir: "app"
+        },
+        // tunnel: true,
+        // host: 'localhost'
+    })
+});
+
+gulp.task('autoprefixer', function () {
+    gulp.src('app/css/style.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('app/css'))
+});
+
+gulp.task('watch', ['runBrSynk', 'scss', 'autoprefixer'], function () {
+    gulp.watch('app/scss/*.scss', ['scss']);
+    gulp.watch('app/scss/*.scss', ['autoprefixer']);
+    gulp.watch('app/*.html', browserSync.reload);
+});
